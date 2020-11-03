5c4f02bff6732cba9a6e40c7ba046150fbcc0082
Natashi <32347635+Natashi@users.noreply.github.com>
2020-11-03 09:44:30 +0700

    Player hitbox task

--- ProgFund_GameSFML/source/Game/Player.cpp
+++ ProgFund_GameSFML/source/Game/Player.cpp
@@ -4,15 +4,19 @@
 //*******************************************************************
 //Stage_PlayerTask
 //*******************************************************************
-Stage_PlayerTask::Stage_PlayerTask(Scene* parent) : TaskBase(parent) {
+CONSTRUCT_TASK(Stage_PlayerTask) {
 	moveAngle_ = 0;
 	moveSpeed_ = 0;
+	moveSpeedX_ = 0;
+	moveSpeedY_ = 0;
 
 	playerPos_ = D3DXVECTOR2(0, 0);
 	rcClip_ = DxRect<int>(0, 0, 640, 480);
 
 	frameAnimLeft_ = 0;
 	frameAnimRight_ = 0;
+
+	bFocus_ = false;
 
 	{
 		GET_INSTANCE(ResourceManager, resourceManager);
@@ -28,61 +32,93 @@ Stage_PlayerTask::Stage_PlayerTask(Scene* parent) : TaskBase(parent) {
 Stage_PlayerTask::~Stage_PlayerTask() {
 }
 
-void Stage_PlayerTask::Render() {
-	objSprite_.Render();
-}
-void Stage_PlayerTask::Update() {
+void Stage_PlayerTask::_Move() {
 	GET_INSTANCE(InputManager, inputManager);
 
-	//Update movement
+	double sx = 0;
+	double sy = 0;
+
+	KeyState stateSlow = inputManager->GetKeyState(VirtualKey::Focus);
+	bFocus_ = stateSlow == KeyState::Push || stateSlow == KeyState::Hold;
+
+	if (DIJOYSTATE* statePad = inputManager->GetPadState(0)) {
+		if (statePad->lX != 0 || statePad->lY != 0) {
+			double rateX = std::clamp<double>(statePad->lX / 750.0, -1, 1);		//Left analog X
+			double rateY = std::clamp<double>(statePad->lY / 750.0, -1, 1);		//Left analog Y
+
+			double speedMax = bFocus_ ? SPEED_SLOW : SPEED_FAST;
+
+			sx = speedMax * rateX;
+			sy = speedMax * rateY;
+
+			moveSpeed_ = hypot(sx, sy);
+			if (moveSpeed_ != 0)
+				moveAngle_ = hypot(sy, sx);
+		}
+	}
+
 	{
 		KeyState stateLeft = inputManager->GetKeyState(VirtualKey::Left);
 		KeyState stateRight = inputManager->GetKeyState(VirtualKey::Right);
 		KeyState stateUp = inputManager->GetKeyState(VirtualKey::Up);
 		KeyState stateDown = inputManager->GetKeyState(VirtualKey::Down);
-		KeyState stateSlow = inputManager->GetKeyState(VirtualKey::Focus);
-
-		double sx = 0;
-		double sy = 0;
-		double speed = stateSlow == KeyState::Push || stateSlow == KeyState::Hold
-			? 1.75 : 4;
-		//speed /= 2;
 
 		bool bKeyLeft = stateLeft == KeyState::Push || stateLeft == KeyState::Hold;
 		bool bKeyRight = stateRight == KeyState::Push || stateRight == KeyState::Hold;
 		bool bKeyUp = stateUp == KeyState::Push || stateUp == KeyState::Hold;
 		bool bKeyDown = stateDown == KeyState::Push || stateDown == KeyState::Hold;
-		if (bKeyLeft && !bKeyRight) sx -= speed;
-		if (!bKeyLeft && bKeyRight) sx += speed;
-		if (bKeyUp && !bKeyDown) sy -= speed;
-		if (!bKeyUp && bKeyDown) sy += speed;
 
-		moveSpeed_ = speed;
-		if (sx != 0 && sy != 0) {	//Normalize axes
-			constexpr double diagFactor = 1.0 / GM_SQRT2;
-			sx *= diagFactor;
-			sy *= diagFactor;
+		if (bKeyLeft || bKeyRight || bKeyUp || bKeyDown) {
+			if (bKeyLeft || bKeyRight) sx = 0;
+			if (bKeyUp || bKeyDown) sy = 0;
 
-			if (sx > 0)
-				moveAngle_ = sy > 0 ? Math::DegreeToRadian(45) : Math::DegreeToRadian(315);
-			else 
-				moveAngle_ = sy > 0 ? Math::DegreeToRadian(135) : Math::DegreeToRadian(225);
-		}
-		else if (sx != 0 || sy != 0) {
-			if (sx != 0) 
-				moveAngle_ = sx > 0 ? Math::DegreeToRadian(0) : Math::DegreeToRadian(180);
-			else if (sy != 0) 
-				moveAngle_ = sy > 0 ? Math::DegreeToRadian(90) : Math::DegreeToRadian(270);
-		}
-		else {
-			moveSpeed_ = 0;
-		}
+			double speed = bFocus_ ? SPEED_SLOW : SPEED_FAST;
 
-		//Add and clip player position
-		{
-			playerPos_.x = std::clamp<float>(playerPos_.x + sx, rcClip_.left, rcClip_.right);
-			playerPos_.y = std::clamp<float>(playerPos_.y + sy, rcClip_.top, rcClip_.bottom);
+			if (bKeyLeft && !bKeyRight) sx -= speed;
+			if (!bKeyLeft && bKeyRight) sx += speed;
+			if (bKeyUp && !bKeyDown) sy -= speed;
+			if (!bKeyUp && bKeyDown) sy += speed;
+
+			moveSpeed_ = speed;
+			if (sx != 0 && sy != 0) {	//Normalize axes
+				constexpr double diagFactor = 1.0 / GM_SQRT2;
+				sx *= diagFactor;
+				sy *= diagFactor;
+
+				if (sx > 0)
+					moveAngle_ = sy > 0 ? Math::DegreeToRadian(45) : Math::DegreeToRadian(315);
+				else
+					moveAngle_ = sy > 0 ? Math::DegreeToRadian(135) : Math::DegreeToRadian(225);
+			}
+			else if (sx != 0 || sy != 0) {
+				if (sx != 0)
+					moveAngle_ = sx > 0 ? Math::DegreeToRadian(0) : Math::DegreeToRadian(180);
+				else if (sy != 0)
+					moveAngle_ = sy > 0 ? Math::DegreeToRadian(90) : Math::DegreeToRadian(270);
+			}
+			else {
+				moveSpeed_ = 0;
+			}
 		}
+	}
+
+	moveSpeedX_ = sx;
+	moveSpeedY_ = sy;
+
+	//Add and clip player position
+	{
+		playerPos_.x = std::clamp<float>(playerPos_.x + sx, rcClip_.left, rcClip_.right);
+		playerPos_.y = std::clamp<float>(playerPos_.y + sy, rcClip_.top, rcClip_.bottom);
+	}
+}
+
+void Stage_PlayerTask::Render() {
+	objSprite_.Render();
+}
+void Stage_PlayerTask::Update() {
+	//Update movement
+	{
+		_Move();
 		objSprite_.SetPosition(roundf(playerPos_.x), roundf(playerPos_.y), 1.0f);
 
 		//Update player animation
@@ -99,7 +135,7 @@ void Stage_PlayerTask::Update() {
 			static const int resetFrameLeft = 4 * FRAME_M;
 			static const int resetFrameRight = 4 * FRAME_M;
 
-			if (bKeyLeft) {
+			if (moveSpeedX_ < 0) {
 				if (frameAnimLeft_ < countSpriteLeft * FRAME_M - 1)
 					++frameAnimLeft_;
 				else
@@ -111,7 +147,7 @@ void Stage_PlayerTask::Update() {
 				frameAnimLeft_ -= 2;
 			}
 
-			if (bKeyRight) {
+			if (moveSpeedX_ > 0) {
 				if (frameAnimRight_ < countSpriteRight * FRAME_M - 1)
 					++frameAnimRight_;
 				else
@@ -146,6 +182,108 @@ void Stage_PlayerTask::Update() {
 			}
 		}
 	}
+
+	{
+		GET_INSTANCE(InputManager, inputManager);
+
+		KeyState stateSlow = inputManager->GetKeyState(VirtualKey::Focus);
+
+		if (stateSlow == KeyState::Push || stateSlow == KeyState::Hold) {
+			if (taskHitbox1_ == nullptr) {
+				taskHitbox1_ = shared_ptr<Stage_PlayerHitboxTask>(
+					new Stage_PlayerHitboxTask(parent_, 255, 160, 0.3, 12, -180, 180, 3));
+				taskHitbox2_ = shared_ptr<Stage_PlayerHitboxTask>(
+					new Stage_PlayerHitboxTask(parent_, 0, 255, 1.2, 18, 0, 0, -2));
+
+				parent_->AddTask(taskHitbox1_);
+				parent_->AddTask(taskHitbox2_);
+			}
+		}
+		else if (stateSlow == KeyState::Pull || stateSlow == KeyState::Free) {
+			if (taskHitbox1_ != nullptr) {
+				taskHitbox1_->bEnd_ = true;
+				taskHitbox2_->bEnd_ = true;
+
+				taskHitbox1_ = nullptr;		//Removes a reference
+				taskHitbox2_ = nullptr;
+			}
+		}
+	}
+
+	++frame_;
+}
+
+//*******************************************************************
+//Stage_PlayerHitboxTask
+//*******************************************************************
+Stage_PlayerHitboxTask::Stage_PlayerHitboxTask(Scene* parent, int s_alpha, int e_alpha, double s_scale,
+	size_t s_timer, double s_angle, double e_angle, double r_spin) : TaskBase(parent)
+{
+	frameSub_ = 0;
+	bEnd_ = false;
+
+	iniAlpha_ = s_alpha;
+	endAlpha_ = e_alpha;
+	iniScale_ = s_scale;
+	scaleTimer_ = s_timer;
+	iniAngle1_ = s_angle;
+	iniAngle2_ = e_angle;
+	spinRate_ = r_spin;
+
+	tScale_ = iniScale_;
+	tAngle_ = iniAngle1_;
+
+	{
+		GET_INSTANCE(ResourceManager, resourceManager);
+
+		auto textureHitbox = resourceManager->GetResourceAs<TextureResource>("img/player/eff_sloweffect.png");
+
+		objHitbox_.SetTexture(textureHitbox);
+		objHitbox_.SetSourceRect(DxRect(0, 0, 63, 63));
+		objHitbox_.SetDestCenter();
+		objHitbox_.UpdateVertexBuffer();
+	}
+
+	objHitbox_.SetAlpha(iniAlpha_);
+	objHitbox_.SetScale(tScale_, tScale_, 1.0f);
+	objHitbox_.SetAngleZ(Math::DegreeToRadian(tAngle_));
+}
+
+void Stage_PlayerHitboxTask::Render() {
+	objHitbox_.Render();
+}
+void Stage_PlayerHitboxTask::Update() {
+	if (!bEnd_) {
+		if (frame_ < 18) {
+			tScale_ = Math::Lerp::Smooth<double>(iniScale_, 1, std::min(frame_, scaleTimer_) / (double)scaleTimer_);
+			tAngle_ = Math::Lerp::Linear(iniAngle1_, iniAngle2_, frame_ / 17.0);
+
+			objHitbox_.SetAlpha(Math::Lerp::Linear(iniAlpha_, endAlpha_, std::min(frame_, 6U) / 6.0));
+		}
+		else {
+			if (frame_ == 18) objHitbox_.SetAlpha(endAlpha_);
+			tAngle_ += spinRate_;
+		}
+	}
+	else {
+		static double lScale;
+		if (frameEnd_ == UINT_MAX) {
+			frameEnd_ = 6;
+			frame_ = 0;
+			lScale = tScale_;
+		}
+
+		tScale_ = Math::Lerp::Accelerate<double>(lScale, 0, frame_ / 5.0);
+		tAngle_ += spinRate_;
+	}
+
+	{
+		auto objPlayer = ((Stage_MainScene*)parent_)->GetPlayerTask();
+		objHitbox_.SetX(roundf(objPlayer->GetX()));
+		objHitbox_.SetY(roundf(objPlayer->GetY()));
+	}
+	objHitbox_.SetScale(tScale_, tScale_, 1.0f);
+	objHitbox_.SetAngleZ(Math::DegreeToRadian(tAngle_));
 
 	++frame_;
 }
--- ProgFund_GameSFML/source/Game/Player.hpp
+++ ProgFund_GameSFML/source/Game/Player.hpp
@@ -4,17 +4,29 @@
 #include "System.hpp"
 #include "StageMain.hpp"
 
+class Stage_PlayerHitboxTask;
 class Stage_PlayerTask : public TaskBase {
+	static constexpr const double SPEED_FAST = 4;
+	static constexpr const double SPEED_SLOW = 1.5;
 private:
 	Sprite2D objSprite_;
 	int frameAnimLeft_;
 	int frameAnimRight_;
+
+	bool bFocus_;
+
+	shared_ptr<Stage_PlayerHitboxTask> taskHitbox1_;
+	shared_ptr<Stage_PlayerHitboxTask> taskHitbox2_;
 
 	D3DXVECTOR2 playerPos_;
 	DxRect<int> rcClip_;
 
 	double moveAngle_;
 	double moveSpeed_;
+	double moveSpeedX_;
+	double moveSpeedY_;
+
+	void _Move();
 public:
 	Stage_PlayerTask(Scene* parent);
 	~Stage_PlayerTask();
@@ -29,4 +41,29 @@ public:
 	D3DXVECTOR2& GetPosition() { return playerPos_; }
 
 	void SetClip(const DxRect<int>& clip) { rcClip_ = clip; }
+};
+class Stage_PlayerHitboxTask : public TaskBase {
+	friend class Stage_PlayerTask;
+private:
+	Sprite2D objHitbox_;
+
+	size_t frameSub_;
+	bool bEnd_;
+
+	int iniAlpha_;
+	int endAlpha_;
+	double iniScale_;
+	size_t scaleTimer_;
+	double iniAngle1_;
+	double iniAngle2_;
+	double spinRate_;
+
+	double tScale_;
+	double tAngle_;
+public:
+	Stage_PlayerHitboxTask(Scene* parent, int s_alpha, int e_alpha, double s_scale, 
+		size_t s_timer, double s_angle, double e_angle, double r_spin);
+
+	virtual void Render();
+	virtual void Update();
 };
